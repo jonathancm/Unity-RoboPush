0bdc83ecc47b6de8d8c1cc7d68deeaefca07410a
Jonathan Caron <jonathancm@outlook.com>
2019-04-05 14:42:40 -0400

    Created Attractor and Repulsor hazards

--- Assets/Scripts/AttractorHazard.cs
+++ Assets/Scripts/AttractorHazard.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using NaughtyAttributes;
 
 public class AttractorHazard : MonoBehaviour
 {
 	// Configurable Parameters
 	[SerializeField] float gravityStrength = 9.81f;
+	[SerializeField] float pulseFrequency = 0.5f;
+
+	// State Variable
+	float pulseStrength = 1.0f;
+
+	private void CalculatePulseStrength()
+	{
+		if(pulseFrequency > 0.0f)
+			pulseStrength = 0.5f * Mathf.Cos(Time.time * pulseFrequency) + 0.5f;
+		else
+			pulseStrength = 1.0f;
+	}
 
 	private void OnTriggerStay(Collider other)
 	{
@@ -13,10 +26,12 @@ public class AttractorHazard : MonoBehaviour
 		if(!otherBody)
 			return;
 
-		// TODO: Implement pulsing force to give a chance to player to release from grasp
+		CalculatePulseStrength();
 
+		// F_grav = 1 / d^2
 		Vector3 pullDirection = transform.position - other.transform.position;
-		float pullStrength = gravityStrength / pullDirection.magnitude;
+		float pullStrength = (gravityStrength * pulseStrength) / (pullDirection.magnitude * pullDirection.magnitude);
+
 		Vector3 pullForce = pullDirection.normalized * pullStrength;
 		otherBody.AddForce(pullForce, ForceMode.Force);
 	}
